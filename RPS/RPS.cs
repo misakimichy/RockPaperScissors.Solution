54c1caa0fd0d8a423cb7b92c24204def56d30939
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-18 15:38:24 -0800

    Separate frontend logic into PlayGame, ErrorMessage and AskPlayAgain methods.

--- RPS/RPS.cs
+++ RPS/RPS.cs
@@ -8,43 +8,56 @@ namespace RPS
         public static void Main() 
         {
             Console.WriteLine("Welcome to 'Rock, Paper, Scissors' game!");
-            while (true)
+            PlayGame();
+        }
+        private static void PlayGame()
+        {
+            Console.Write("Enter [Rock/Paper/Scissors]:");
+            string input = Console.ReadLine();
+            string strFirstLetter = input.Substring(0, 1).ToUpper();
+            string userInput = strFirstLetter + input.Substring(1);
+            string[] choices = {"Rock", "Paper", "Scissors"};
+            // If the input is not "rock", "Paper" nor "Scissors"
+            if (!Array.Exists(choices, element => element == userInput))
             {
-                bool flag = false;
-                Console.Write("Enter [Rock/Paper/Scissors]:");
-                string userInput = Console.ReadLine();
-                string[] choices = {"Rock", "Paper", "Scissors"};
-                if (!Array.Exists(choices, element => element == userInput))
-                {
-                    Console.WriteLine("Invalid input");
-                    continue;
-                }
-                
-                string computerChoice = Game.RandomizeChoice();
-                Console.WriteLine("Computer hand: " + computerChoice);
-                Console.WriteLine($"Result of game: {Game.CheckWinner(userInput, computerChoice)}");
-                while (true)
-                {
-                    Console.WriteLine("Do you want to play a game [yes/no]:");
-                    string userInput2 = Console.ReadLine();
-                    if (userInput2 == "no") 
-                    {
-                        flag = true;
-                        break;
-                    }
-                    else if (userInput2 == "yes")
-                    {
-                        break;
-                    }
-
-                }
-                if (flag)
-                {
-                    break;
-                }
-
+                ErrorMessage();
+                PlayGame();
             }
+            else
+            {
+                string computerChoice = Game.RandomizeChoice();
+                Console.WriteLine("\nComputer hand: " + computerChoice);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"\nResult of game: {Game.CheckWinner(userInput, computerChoice)}\n");
+                Console.ResetColor();
+                AskPlayAgain();
+            }
+        }
+        private static void ErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input");
+            Console.ResetColor();
+        }
 
+        private static void AskPlayAgain()
+        {
+            Console.WriteLine("Do you want to play again? [yes/no]:");
+            string userInput = Console.ReadLine();
+            if (userInput == "no")
+            {
+                Console.WriteLine("Thank you for playing with us!");
+                return;
+            }
+            else if (userInput == "yes")
+            {
+                PlayGame();
+            }
+            else
+            {
+                ErrorMessage();
+                AskPlayAgain();
+            }
         }
     }
 }
